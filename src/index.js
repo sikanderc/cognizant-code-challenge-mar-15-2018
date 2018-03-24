f9bf00d776154eda4e3c9086ef94311e78667aa9
sikanderc <sikanderchowhan@gmail.com>
2018-03-23 23:32:34 -0400

    some changes to functionality, testing to see if there is an improvement or not

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const neoInstancePlanet = 'Juptr'
 const today = new Date()
 let arr = []
 let top3 = []
+let res
 
 document.addEventListener('DOMContentLoaded', function() {
   display3closest()
@@ -27,37 +28,72 @@ function aggregateInfo(json) {
       neoInstanceDate = new Date(obj.close_approach_date)
       timeFromNow = neoInstanceDate - today
       if ((obj.orbiting_body === neoInstancePlanet) && (timeFromNow > 0)) {
-        let newObj = {...currentNeo, close_approach_data: obj, time_from_now: timeFromNow}
+        let newObj = {name: currentNeo.name, id: currentNeo.neo_reference_id, time_from_now: timeFromNow, orbital_period: currentNeo.orbital_data.orbital_period, diameter: ((currentNeo.estimated_diameter.feet.estimated_diameter_min + currentNeo.estimated_diameter.feet.estimated_diameter_max)/2), inclination: currentNeo.orbital_data.inclination, eccentricity: currentNeo.orbital_data.eccentricity}
         return arr.push(newObj)
       }
     })
   })
 }
 
-async function sortDates() {
+async function getMinTime() {
   const fetching = await fetchURL()
-  arr.sort((a,b) => {
-    a.time_from_now - b.time_from_now
-  })
-  console.log("sorted");
+
+  for (var j = 0; j < 3; j++) {
+    res = Math.min.apply(Math, arr.map((o) => {
+      console.log(o);
+      console.log(o.timeFromNow);
+      o.timeFromNow
+    }))
+    console.log(res);
+    let littleNeo = arr.splice(arr.indexOf(res))
+    top3.push(littleNeo)
+  }
+  console.log("got top 3");
 }
 
 async function display3closest() {
-  const sorting = await sortDates()
+  const gettingMinTime = await getMinTime()
   console.log("displaying");
 
-  for (var j = 0; j < 3; j++) {
-    top3.push(arr.shift())
-  }
   let displayNeos = document.getElementById('neos')
-  displayNeos.innerHTML = ''
+  displayNeos.innerHTML = `<tr>
+    <td>Name</td>
+    <td>Orbital Period</td>
+    <td>Avg. Diameter (in feet)</td>
+    <td>Inclination</td>
+    <td>Eccentricity</td>
+  </tr>`
   top3.forEach(neo => {
     displayNeos.innerHTML += `<tr>
     <td>${neo.name}</td>
-    <td>${neo.orbital_data.orbital_period}</td>
-    <td>${neo.estimated_diameter.feet.estimated_diameter_min}</td>
-    <td>${neo.orbital_data.inclination}</td>
-    <td>${neo.orbital_data.eccentricity}</td>
+    <td>${neo.orbital_period}</td>
+    <td>${neo.diameter}</td>
+    <td>${neo.inclination}</td>
+    <td>${neo.eccentricity}</td>
   </tr>`
   })
 }
+
+// function lowestOrbitalPeriod() {
+//   let lowestOrb = Math.min.apply(Math, top3.map((o) => {
+//     o.orbital_period
+//   }))
+// }
+//
+// function smallestDiameter() {
+//   let smallestDiam = Math.min.apply(Math, top3.map((o) => {
+//     o.diameter
+//   }))
+// }
+//
+// function highestInclination() {
+//   let highestIncl = Math.max.apply(Math, top3.map((o) => {
+//     o.inclination
+//   }))
+// }
+//
+// function lowestEccentricity() {
+//   let lowestEccen = Math.min.apply(Math, top3.map((o) => {
+//     o.eccentricity
+//   }))
+// }
